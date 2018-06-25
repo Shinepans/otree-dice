d3c2615e1df83c3ece1e9bb432d10bbf97ada292
shinepans <shinepans@live.com>
2018-06-25 15:51:27 +0800

    dicev01

---
+++ dice/pages.py
@@ -0,0 +1,28 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+from otree.forms.widgets import _CurrencyInput
+
+class Instructions(Page):
+    def vars_for_template(self):
+        return {'curname':_CurrencyInput.CURRENCY_SYMBOL}
+
+
+class DiceRolling(Page):
+    form_model = 'player'
+    form_fields = ['dice', 'report']
+
+    def before_next_page(self):
+        self.player.set_payoff()
+
+class FinalResults(Page):
+    ...
+
+class DiceRollingResults(Page):
+    ...
+
+page_sequence = [
+    Instructions,
+    DiceRolling,
+    DiceRollingResults
+]
